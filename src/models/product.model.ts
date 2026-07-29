c26c648a2f5b81d5b3b5cf62f3e1a00ae0ef7806
Manyata Roka <161047309+JuniorManyata@users.noreply.github.com>
2026-07-29 08:01:15 +0545

    Update

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,27 +1,27 @@
-import mongoose, { Document, Schema } from "mongoose";
-import { ProductType } from "../types/product.type";
+// import mongoose, { Document, Schema } from "mongoose";
+// import { ProductType } from "../types/product.type";
 
-const ProductSchema: Schema = new Schema<ProductType>(
-    {
-        name: { type: String, required: true, unique: true },
-        description: { type: String, default: "" },
-        price: { type: Number, required: true },
-        imagePath: { type: String },
-        category: { type: String, required: true },
-        stock: { type: Number, default: 0 },
-        isFavorited: { type: Boolean, default: false },
-        trending: { type: Boolean, default: false },
-        isNewArrival: { type: Boolean, default: false },
-    },
-    {
-        timestamps: true,
-    }
-);
+// const ProductSchema: Schema = new Schema<ProductType>(
+//     {
+//         name: { type: String, required: true, unique: true },
+//         description: { type: String, default: "" },
+//         price: { type: Number, required: true },
+//         imagePath: { type: String },
+//         category: { type: String, required: true },
+//         stock: { type: Number, default: 0 },
+//         isFavorited: { type: Boolean, default: false },
+//         trending: { type: Boolean, default: false },
+//         isNewArrival: { type: Boolean, default: false },
+//     },
+//     {
+//         timestamps: true,
+//     }
+// );
 
-export interface IProduct extends ProductType, Document {
-    _id: mongoose.Types.ObjectId;
-    createdAt: Date;
-    updatedAt: Date;
-}
+// export interface IProduct extends ProductType, Document {
+//     _id: mongoose.Types.ObjectId;
+//     createdAt: Date;
+//     updatedAt: Date;
+// }
 
-export const ProductModel = mongoose.model<IProduct>('Product', ProductSchema);
+// export const ProductModel = mongoose.model<IProduct>('Product', ProductSchema);
