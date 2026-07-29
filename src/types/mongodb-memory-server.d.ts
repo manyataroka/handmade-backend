701073a71b33d5596a9d84bccfecc934ef04e974
Manyata Roka <161047309+JuniorManyata@users.noreply.github.com>
2026-07-29 07:59:11 +0545

    Update

--- src/types/mongodb-memory-server.d.ts
+++ src/types/mongodb-memory-server.d.ts
@@ -1,9 +1,9 @@
-// declare module 'mongodb-memory-server' {
-//   export class MongoMemoryServer {
-//     static create(opts?: any): Promise<MongoMemoryServer>;
-//     getUri(): string;
-//     stop(): Promise<void>;
-//   }
-//   export { MongoMemoryServer };
-//   export default MongoMemoryServer;
-// }
+declare module 'mongodb-memory-server' {
+  export class MongoMemoryServer {
+    static create(opts?: any): Promise<MongoMemoryServer>;
+    getUri(): string;
+    stop(): Promise<void>;
+  }
+  export { MongoMemoryServer };
+  export default MongoMemoryServer;
+}
